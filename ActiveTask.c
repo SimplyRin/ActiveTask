20359db53f8f214f95eea967f7f97edf8f6af531
SimplyRin <simplyrin.dev@gmail.com>
2018-07-09 00:23:25 +0900

    Update ActiveTask.c

--- ActiveTask.c
+++ ActiveTask.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
 	char wnd_title[256];
-	HWND hwnd = GetForegroundWindow(); // get handle of currently active window
+	HWND hwnd = GetForegroundWindow();
 	GetWindowText(hwnd,wnd_title,sizeof(wnd_title));
 
 	printf(wnd_title);
